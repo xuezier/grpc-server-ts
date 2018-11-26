13b308f7f9fc735d210e964a05aa2ff16c00959e
tunan <tunan@gaoding.com>
2018-11-26 09:39:40 +0800

    fix: fix no paramter passed for setting decorator

--- src/decorator/Settings.ts
+++ src/decorator/Settings.ts
@@ -2,7 +2,10 @@ import { Settings } from '../interface/Settings';
 import { SettingRegistry } from '../SettingRegistry';
 
 export function Settings(settings: Settings) {
-  return function (target: Function) {
+  return function(target: any) {
+    if (!(target instanceof Function)) {
+      throw new Error('setting target must be a function');
+    }
     SettingRegistry.registry(settings);
   };
 }
